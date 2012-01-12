24384cc2eedd14a2fae0e40de4b2dca1b042e30f
David Stryker <dstryker@ning.com>
2012-01-12 10:19:24 -0800

    Add a TODO

--- util/src/main/java/com/ning/arecibo/util/timeline/ScalarSample.java
+++ util/src/main/java/com/ning/arecibo/util/timeline/ScalarSample.java
@@ -1,8 +1,6 @@
 package com.ning.arecibo.util.timeline;
 
 /**
- * TODO: sampleValues will be boxed, and that sucks.  But we can replace
- * with 8 opcode-specific classes extending SampleBase.
  * @param <T> A value consistent with the opcode
  */
 public class ScalarSample<T> extends SampleBase {
