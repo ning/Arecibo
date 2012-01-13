90ea299f88de539a614ed18c80483c4717bc9ba3
Pierre-Alexandre Meyer <pierre@ning.com>
2012-01-13 15:03:34 -0800

    event: fix UDP config
    Cache correctly the UDP port for successive calls.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- event/src/main/java/com/ning/arecibo/event/receiver/UDPEventReceiverConfig.java
+++ event/src/main/java/com/ning/arecibo/event/receiver/UDPEventReceiverConfig.java
@@ -1,13 +1,16 @@
 package com.ning.arecibo.event.receiver;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
 import org.skife.config.Config;
 import org.skife.config.Default;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+
 public abstract class UDPEventReceiverConfig
 {
+    int port = -1;
+
     @Config("arecibo.host")
     @Default("0.0.0.0")
     public abstract String getHost();
@@ -23,11 +26,16 @@ public abstract class UDPEventReceiverConfig
     public int getPort()
     {
         if ("auto".equals(getPortValue())) {
+            if (port != -1) {
+                return port;
+            }
+
             try {
                 ServerSocket sock = new ServerSocket();
                 sock.bind(new InetSocketAddress(0));
                 int value = sock.getLocalPort();
                 sock.close();
+                port = value;
                 return value;
             }
             catch (IOException ex) {
