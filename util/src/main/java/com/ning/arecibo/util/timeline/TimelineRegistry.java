1c5dad895dcbc918a6d5d567e8699167436c9763
Pierre-Alexandre Meyer <pierre@ning.com>
2012-01-17 13:24:55 -0800

    util: synchronize TimelineRegistry
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- util/src/main/java/com/ning/arecibo/util/timeline/TimelineRegistry.java
+++ util/src/main/java/com/ning/arecibo/util/timeline/TimelineRegistry.java
@@ -3,41 +3,54 @@ package com.ning.arecibo.util.timeline;
 import com.google.common.collect.BiMap;
 import com.google.inject.Inject;
 
-// TODO: There is no synchronization: Either prove caller is single-threaded or add synchronization.
-public class TimelineRegistry {
+public class TimelineRegistry
+{
+    private final Object hostsMonitor = new Object();
+    private final Object sampleKindsMonitor = new Object();
 
     private final TimelineDAO dao;
     private final BiMap<Integer, String> hosts;
     private final BiMap<Integer, String> sampleKinds;
 
     @Inject
-    public TimelineRegistry(TimelineDAO dao) {
+    public TimelineRegistry(final TimelineDAO dao)
+    {
         this.dao = dao;
         this.hosts = dao.getHosts();
         this.sampleKinds = dao.getSampleKinds();
     }
 
-    public int getOrAddHost(final String host) {
-        final Integer hostId = hosts.inverse().get(host);
-        if (hostId != null) {
-            return hostId;
+    public int getOrAddHost(final String host)
+    {
+        Integer hostId = hosts.inverse().get(host);
+
+        if (hostId == null) {
+            synchronized (hostsMonitor) {
+                hostId = hosts.inverse().get(host);
+                if (hostId == null) {
+                    hostId = dao.addHost(host);
+                    hosts.put(hostId, host);
+                }
+            }
         }
-        else {
-            final int newHostId = dao.addHost(host);
-            hosts.put(newHostId, host);
-            return newHostId;
-        }
+
+        return hostId;
     }
 
-    public int getOrAddSampleKind(final String sampleKind) {
-        final Integer sampleKindId = sampleKinds.inverse().get(sampleKind);
-        if (sampleKindId != null) {
-            return sampleKindId;
+    public int getOrAddSampleKind(final String sampleKind)
+    {
+        Integer sampleKindId = sampleKinds.inverse().get(sampleKind);
+
+        if (sampleKindId == null) {
+            synchronized (sampleKindsMonitor) {
+                sampleKindId = sampleKinds.inverse().get(sampleKind);
+                if (sampleKindId == null) {
+                    sampleKindId = dao.addSampleKind(sampleKind);
+                    sampleKinds.put(sampleKindId, sampleKind);
+                }
+            }
         }
-        else {
-            final int newSampleKindId = dao.addSampleKind(sampleKind);
-            sampleKinds.put(newSampleKindId, sampleKind);
-            return newSampleKindId;
-        }
+
+        return sampleKindId;
     }
 }
