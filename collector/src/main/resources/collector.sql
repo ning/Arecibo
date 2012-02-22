d88853ff4187ffdc512c72d1ecd24813596a39ed
Pierre-Alexandre Meyer <pierre@ning.com>
2012-02-22 13:26:58 -0800

    collector: tweak MySQL indexes
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- collector/src/main/resources/collector.sql
+++ collector/src/main/resources/collector.sql
@@ -3,7 +3,7 @@ create table hosts (
    host_id int not null auto_increment primary key,
    host_name varchar(256) not null,
    created_dt int not null,
-   key idx_host(host_id),
+   key idx_host(host_name),
    key idx_created_dt_host(created_dt, host_id)
 )
 ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -22,8 +22,7 @@ create table timeline_times (
    end_time int not null,
    count int not null,
    times mediumblob not null,
-   key idx_host_id_start_time(host_id, start_time),
-   key idx_host_id_end_time(host_id, end_time)
+   key idx_host_id_start_end_time(host_id, start_time, end_time)
 )
 ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
