d2090c3d05355d65e1d049d03937612e0eb395e9
Pierre-Alexandre Meyer <pierre@ning.com>
2012-03-01 11:10:33 -0800

    dashboard: remove unused classes and enums
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- dashboard/src/main/java/com/ning/arecibo/dashboard/data/DataHierarchyHandler.java
+++
@@ -1,9 +0,0 @@
-package com.ning.arecibo.dashboard.data;
-
-import java.util.List;
-import java.io.IOException;
-import javax.servlet.http.HttpServletRequest;
-
-public interface DataHierarchyHandler {
-    public String getDataHierarchyResponse(HttpServletRequest request, List<String> tokens) throws IOException;
-}
