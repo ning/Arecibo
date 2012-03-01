2a123bfcdc7ce8ee011b9fa1f30138e06d642545
Pierre-Alexandre Meyer <pierre@ning.com>
2012-03-01 11:12:00 -0800

    dashboard: update default configuration
    * Remove unused options
    * Make alert discovery service off by default
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- dashboard/src/main/java/com/ning/arecibo/dashboard/guice/DashboardConfig.java
+++ dashboard/src/main/java/com/ning/arecibo/dashboard/guice/DashboardConfig.java
@@ -7,49 +7,13 @@ import org.skife.config.TimeSpan;
 
 public interface DashboardConfig
 {
-    @Config("arecibo.dashboard.collector_cache_size")
-    @Default("1024")
-    int getCollectorCacheSize();
-
-    @Config("arecibo.dashboard.collector_cache_max_wait")
-    @Default("1m")
-    TimeSpan getCollectorCacheMaxWait();
-
-    @Config("arecibo.dashboard.collector_cache_time_to_live")
-    @Default("2m")
-    TimeSpan getCollectorCacheTimeToLive();
-
-    @Config("arecibo.dashboard.table_cache_size")
-    @Default("1024")
-    int getTableCacheSize();
-
     @Config("arecibo.dashboard.alert_host_override")
     @DefaultNull
     String getAlertHostOverride();
 
     @Config("arecibo.dashboard.alert_manager_enabled")
-    @Default("true")
+    @Default("false")
     boolean isAlertManagerEnabled();
-
-    @Config("arecibo.dashboard.collector_host")
-    @Default("127.0.0.1")
-    String getCollectorHost();
-
-    @Config("arecibo.dashboard.dashboard_collector_keep_alive_host")
-    @DefaultNull
-    String getDashboardCollectorKeepAliveHost();
-
-    @Config("arecibo.collector.service_name")
-    @Default("EventCollectorServer")
-    String getCollectorServiceName();
-
-    @Config("arecibo.dashboard.e2ez_threshold_config_cache_size")
-    @Default("1024")
-    int getE2EZThresholdConfigCacheSize();
-
-    @Config("arecibo.dashboard.e2ez_time_range_map_cache_size")
-    @Default("64")
-    int getE2EZTimeRangeMapCacheSize();
 
     @Config("arecibo.dashboard.serviceLocatorKlass")
     @Default("com.ning.arecibo.util.service.DummyServiceLocator")
