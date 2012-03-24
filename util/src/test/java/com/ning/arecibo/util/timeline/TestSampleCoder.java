ning/Arecibo

commit 32ee3810b9e45e0205dc0034b8aac661e66ed3e8

collector: fix SQL statements
This also adds a missing copyright header and fixes the expected output
of a unit test.

Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

collector/src/test/java/com/ning/arecibo/collector/TestTimelineChunkAndTimes.java
@@ -68,9 +68,9 @@ public class TestTimelineChunkAndTimes
         Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(null, null), "1326749039,12345,1326749040,12346,1326749041,12347");
         Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime, null), "1326749039,12345,1326749040,12346,1326749041,12347");
         Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(null, startTime.plusSeconds(sampleCount)), "1326749039,12345,1326749040,12346,1326749041,12347");
-        Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(1), startTime.plusSeconds(sampleCount)), "1326749039,12345,1326749040,12346,1326749041,12347");
-        Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(2), startTime.plusSeconds(sampleCount)), "1326749040,12346,1326749041,12347");
-        Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(3), startTime.plusSeconds(sampleCount)), "1326749041,12347");
+        Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(1), startTime.plusSeconds(sampleCount)), "1326749040,12346,1326749041,12347");
+        Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(2), startTime.plusSeconds(sampleCount)), "1326749041,12347");
+        Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(3), startTime.plusSeconds(sampleCount)), "");
         Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(4), startTime.plusSeconds(sampleCount)), "");
         // Buggy start date
         Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(10), startTime.plusSeconds(sampleCount)), "");

collector/src/main/java/com/ning/arecibo/collector/persistent/TimelineAggregator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010-2012 Ning, Inc.
+ *
+ * Ning licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.ning.arecibo.collector.persistent;
 
 import java.sql.ResultSet;

util/src/main/java/com/ning/arecibo/util/timeline/DefaultTimelineDAO.java
@@ -311,7 +311,7 @@ public class DefaultTimelineDAO implements TimelineDAO
                             "join timeline_times t using (timeline_times_id)\n" +
                             "where t.start_time >= :start_time\n" +
                             "and t.end_time <= :end_time\n" +
-                            "and t.not_valid = 0" +
+                            "and t.not_valid = 0\n" +
                             "and h.host_name = :host_name\n" +
                             "order by start_time asc\n" +
                             ";")
@@ -354,7 +354,7 @@ public class DefaultTimelineDAO implements TimelineDAO
                             "and t.end_time <= :end_time\n" +
                             "and h.host_name = :host_name\n" +
                             "and k.sample_kind = :sample_kind\n" +
-                            "and t.not_valid = 0" +
+                            "and t.not_valid = 0\n" +
                             "order by start_time asc\n" +
                             ";")
                     .bind("host_name", hostName)

commit 2989a11b98504e68dbe9cd9feb971fa3f72af0de

util: fix a few issues related to timeline times encoding
In TimeRangeSampleProcessor#processSamples, operate only on epoch
to do time comparisons, to avoid time zones issues (TimelineTimes.unixSeconds()
will convert to GMT).

In TimelineChunkAccumulator, make sure deepCopy() doesn't hold a reference
to the original stream.

In TimelineCoder#compressTimes(), allow for repeated samples. I don't believe
the collector code prevents receiving two samples at the same time
(TimelineHostEventAccumulator#addHostSamples() will just check that any new event
has not strictly happened before any other samples received).

In TimelineCoder#writeRepeatedDelta(), handle 1-time "repeated" delta.

These changes make the TestCollectorEventProcessor#testCache() pass again. I've also added
TestSampleCoder and enhanced TestTimelineCoder tests to verify these fixes.

Fix a regression introduced in 32ee3810b9e45e0205dc0034b8aac661e66ed3e8 for
TestTimelineChunkAndTimes#testToString test.

I've also slightly refactored TimelineEventHandler#getInMemoryTimelineChunkAndTimes().

Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

collector/src/main/java/com/ning/arecibo/collector/persistent/TimelineEventHandler.java
@@ -202,29 +202,29 @@ public class TimelineEventHandler implements EventHandler
         // Now, filter each accumulator for this host
         final List<TimelineChunkAndTimes> samplesByHostName = new ArrayList<TimelineChunkAndTimes>();
         for (final TimelineHostEventAccumulator accumulator : hostAccumulators.values()) {
-            // Yup, there is. Check now if the filters apply
             final List<DateTime> accumulatorTimes = accumulator.getTimes();
             final DateTime accumulatorStartTime = accumulator.getStartTime();
             final DateTime accumulatorEndTime = accumulator.getEndTime();
 
+            // Check if the time filters apply
             if ((filterStartTime != null && accumulatorEndTime.isBefore(filterStartTime)) || (filterStartTime != null && accumulatorStartTime.isAfter(filterEndTime))) {
                 // Ignore this accumulator
-                return ImmutableList.of();
+                continue;
             }
 
-            // We have a timeline match, return the samples matching the sample kinds
+            // This accumulator is in the right time range, now return only the sample kinds specified
             for (final TimelineChunkAccumulator chunkAccumulator : accumulator.getTimelines().values()) {
-                // Extract the timeline for this chunk by copying it and reading encoded bytes
-                final TimelineChunkAccumulator chunkAccumulatorCopy = chunkAccumulator.deepCopy();
-                final TimelineChunk timelineChunk = chunkAccumulatorCopy.extractTimelineChunkAndReset(-1, accumulatorStartTime);
-                // NOTE! Further filtering needs to be done in the processing function
-                final TimelineTimes timelineTimes = new TimelineTimes(-1, hostId, accumulatorStartTime, accumulatorEndTime, accumulatorTimes);
-
-                final String sampleKind = timelineDAO.getSampleKind(timelineChunk.getSampleKindId());
+                final String sampleKind = timelineDAO.getSampleKind(chunkAccumulator.getSampleKindId());
                 if (!filterSampleKinds.contains(sampleKind)) {
                     // We don't care about this sample kind
                     continue;
                 }
+
+                // Extract the timeline for this chunk by copying it and reading encoded bytes
+                final TimelineChunkAccumulator chunkAccumulatorCopy = chunkAccumulator.deepCopy();
+                final TimelineChunk timelineChunk = chunkAccumulatorCopy.extractTimelineChunkAndReset(-1, accumulatorStartTime);
+                // NOTE! Further time filtering needs to be done in the processing function
+                final TimelineTimes timelineTimes = new TimelineTimes(-1, hostId, accumulatorStartTime, accumulatorEndTime, accumulatorTimes);
 
                 samplesByHostName.add(new TimelineChunkAndTimes(filterHostName, sampleKind, timelineChunk, timelineTimes));
             }

util/src/main/java/com/ning/arecibo/util/timeline/TimeRangeSampleProcessor.java
@@ -39,14 +39,15 @@ public abstract class TimeRangeSampleProcessor implements SampleProcessor {
      *                     given by the sampleNumber indexing the TimelineTimestamps.
      */
     @Override
-    public void processSamples(final TimeCursor timeCursor, final int sampleCount, final SampleOpcode opcode, final Object value) {
+    public void processSamples(final TimeCursor timeCursor, final int sampleCount, final SampleOpcode opcode, final Object value)
+    {
+        final int unixStartTime = startTime == null ? Integer.MIN_VALUE : TimelineTimes.unixSeconds(startTime);
+        final int unixEndTime = endTime == null ? Integer.MAX_VALUE : TimelineTimes.unixSeconds(endTime);
         for (int i = 0; i < sampleCount; i++) {
-            final DateTime sampleTimestamp = TimelineTimes.dateTimeFromUnixSeconds(timeCursor.getNextTime());
-
             // Check if the sample is in the right time range
-            final long sampleMillis = sampleTimestamp.getMillis();
-            if ((startTime == null || (sampleMillis >= startTime.getMillis())) && (endTime == null || (sampleMillis <= endTime.getMillis()))) {
-                processOneSample(sampleTimestamp, opcode, value);
+            final int unixSampleTime = timeCursor.getNextTime();
+            if (unixSampleTime >= unixStartTime && unixSampleTime <= unixEndTime) {
+                processOneSample(TimelineTimes.dateTimeFromUnixSeconds(unixSampleTime), opcode, value);
             }
         }
     }

util/src/main/java/com/ning/arecibo/util/timeline/TimelineChunkAccumulator.java
@@ -55,20 +55,20 @@ public class TimelineChunkAccumulator
         reset();
     }
 
-    private TimelineChunkAccumulator(final int hostId, final int sampleKindId, final ByteArrayOutputStream stream, final SampleBase lastSample, final int sampleCount) throws IOException
+    private TimelineChunkAccumulator(final int hostId, final int sampleKindId, final byte[] bytes, final SampleBase lastSample, final int sampleCount) throws IOException
     {
         this.hostId = hostId;
         this.sampleKindId = sampleKindId;
-        this.byteStream = stream;
-        byteStream.write(byteStream.toByteArray());
-        outputStream = new DataOutputStream(byteStream);
+
+        reset();
+        this.byteStream.write(bytes);
         this.lastSample = lastSample;
         this.sampleCount = sampleCount;
     }
 
     public TimelineChunkAccumulator deepCopy() throws IOException
     {
-        return new TimelineChunkAccumulator(hostId, sampleKindId, byteStream, lastSample, sampleCount);
+        return new TimelineChunkAccumulator(hostId, sampleKindId, byteStream.toByteArray(), lastSample, sampleCount);
     }
 
     public synchronized void addPlaceholder(final byte repeatCount)

util/src/main/java/com/ning/arecibo/util/timeline/TimelineCoder.java
@@ -16,6 +16,9 @@
 
 package com.ning.arecibo.util.timeline;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
@@ -25,10 +28,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.ning.arecibo.util.Logger;
-
 public class TimelineCoder {
-    public static final Logger log = Logger.getCallersLoggerViaExpensiveMagic();
+    public static final Logger log = LoggerFactory.getLogger(TimelineCoder.class);
     public static final int MAX_DELTA_TIME = 0x7F;
     public static final int MAX_REPEAT_COUNT = 0xFF;
 
@@ -42,8 +43,8 @@ public class TimelineCoder {
             writeTime(0, lastTime, dataStream);
             for (int i=1; i<times.length; i++) {
                 final int newTime = times[i];
-                if (newTime <= lastTime) {
-                    log.warn("In TimelineCoder.compressTimes(), newTime %d is <= lastTime %d; ignored", newTime, lastTime);
+                if (newTime < lastTime) {
+                    log.warn("In TimelineCoder.compressTimes(), newTime {} is < lastTime {}; ignored", newTime, lastTime);
                     continue;
                 }
                 final int delta = newTime - lastTime;
@@ -82,7 +83,7 @@ public class TimelineCoder {
             return outputStream.toByteArray();
         }
         catch (IOException e) {
-            log.error(e, "Exception compressing times array of length %d", times.length);
+            log.error("Exception compressing times array of length {}", times.length, e);
             return null;
         }
     }
@@ -131,10 +132,8 @@ public class TimelineCoder {
     }
 
     private static void writeRepeatedDelta(final int delta, final int repeatCount, final DataOutputStream dataStream) throws IOException {
-        if (repeatCount > 1) {
-            dataStream.writeByte(TimelineOpcode.REPEATED_DELTA_TIME.getOpcodeIndex());
-            dataStream.writeByte(repeatCount);
-        }
+        dataStream.writeByte(TimelineOpcode.REPEATED_DELTA_TIME.getOpcodeIndex());
+        dataStream.writeByte(repeatCount);
         dataStream.writeByte(delta);
     }
 

collector/src/test/java/com/ning/arecibo/collector/TestTimelineChunkAndTimes.java
@@ -68,9 +68,9 @@ public class TestTimelineChunkAndTimes
         Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(null, null), "1326749039,12345,1326749040,12346,1326749041,12347");
         Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime, null), "1326749039,12345,1326749040,12346,1326749041,12347");
         Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(null, startTime.plusSeconds(sampleCount)), "1326749039,12345,1326749040,12346,1326749041,12347");
-        Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(1), startTime.plusSeconds(sampleCount)), "1326749040,12346,1326749041,12347");
-        Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(2), startTime.plusSeconds(sampleCount)), "1326749041,12347");
-        Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(3), startTime.plusSeconds(sampleCount)), "");
+        Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(1), startTime.plusSeconds(sampleCount)), "1326749039,12345,1326749040,12346,1326749041,12347");
+        Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(2), startTime.plusSeconds(sampleCount)), "1326749040,12346,1326749041,12347");
+        Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(3), startTime.plusSeconds(sampleCount)), "1326749041,12347");
         Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(4), startTime.plusSeconds(sampleCount)), "");
         // Buggy start date
         Assert.assertEquals(timelineChunkAndTimes.getSamplesAsCSV(startTime.plusSeconds(10), startTime.plusSeconds(sampleCount)), "");

util/src/test/java/com/ning/arecibo/util/timeline/TestSampleCoder.java
@@ -1,0 +1,91 @@
+/*
+ * Copyright 2010-2012 Ning, Inc.
+ *
+ * Ning licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.ning.arecibo.util.timeline;
+
+import com.google.common.collect.ImmutableList;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TestSampleCoder
+{
+    @Test(groups = "fast")
+    public void testScan() throws Exception
+    {
+        final DateTime startTime = new DateTime(DateTimeZone.UTC);
+        final DateTime endTime = startTime.plusSeconds(5);
+        final TimelineTimes times = new TimelineTimes(-1, -1, startTime, endTime,
+            ImmutableList.<DateTime>of(startTime.plusSeconds(1), startTime.plusSeconds(2), startTime.plusSeconds(3), startTime.plusSeconds(4)));
+
+
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        final DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        final ScalarSample<Short> sample = new ScalarSample<Short>(SampleOpcode.SHORT, (short) 4);
+        SampleCoder.encodeSample(dataOutputStream, sample);
+        SampleCoder.encodeSample(dataOutputStream, new RepeatSample<Short>((byte) 3, sample));
+        dataOutputStream.close();
+
+        SampleCoder.scan(outputStream.toByteArray(), times, new TimeRangeSampleProcessor(startTime, endTime)
+        {
+            @Override
+            public void processOneSample(final DateTime time, final SampleOpcode opcode, final Object value)
+            {
+                Assert.assertTrue(time.isAfter(startTime));
+                Assert.assertTrue(time.isBefore(endTime));
+                Assert.assertEquals(Short.valueOf(value.toString()), sample.getSampleValue());
+            }
+        });
+    }
+
+    @Test(groups = "fast")
+    public void testTimeRangeSampleProcessor() throws Exception
+    {
+        final DateTime startTime = new DateTime("2012-03-23T17:35:11.707Z");
+        final DateTime endTime = new DateTime("2012-03-23T17:35:17.924Z");
+        final int sampleCount = 2;
+
+        final TimelineTimes times = new TimelineTimes(0, -1, startTime, endTime, ImmutableList.<DateTime>of(startTime, endTime));
+        final TimeCursor cursor = times.getTimeCursor();
+        Assert.assertEquals(cursor.getNextTime(), TimelineTimes.unixSeconds(startTime));
+        Assert.assertEquals(cursor.getNextTime(), TimelineTimes.unixSeconds(endTime));
+
+        // 2 x the value 12: REPEAT, SHORT, 2, SHORT, 12 (2 bytes)
+        final byte[] samples = new byte[]{127, 2, 2, 0, 12};
+
+        final AtomicInteger samplesCount = new AtomicInteger(0);
+        SampleCoder.scan(samples, times, new TimeRangeSampleProcessor(startTime, endTime)
+        {
+            @Override
+            public void processOneSample(final DateTime time, final SampleOpcode opcode, final Object value)
+            {
+                if (samplesCount.get() == 0) {
+                    Assert.assertEquals(TimelineTimes.unixSeconds(time), TimelineTimes.unixSeconds(startTime));
+                }
+                else {
+                    Assert.assertEquals(TimelineTimes.unixSeconds(time), TimelineTimes.unixSeconds(endTime));
+                }
+                samplesCount.incrementAndGet();
+            }
+        });
+        Assert.assertEquals(samplesCount.get(), sampleCount);
+    }
+}

util/src/test/java/com/ning/arecibo/util/timeline/TestTimelineCoder.java
@@ -16,21 +16,36 @@
 
 package com.ning.arecibo.util.timeline;
 
-import org.apache.commons.codec.binary.Hex;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TestTimelineCoder {
-    private static int firstTime = 1000000;
-    private static int[] unencodedTimes = new int[] { firstTime, firstTime + 30, firstTime + 60, firstTime + 90, firstTime + 1000, firstTime + 2000, firstTime + 2030, firstTime + 2060 };
+public class TestTimelineCoder
+{
+    @Test(groups = "fast")
+    public void testBasicEncodeDecode() throws Exception
+    {
+        final int firstTime = 1000000;
+        final int[] unencodedTimes = new int[]{firstTime, firstTime + 30, firstTime + 60, firstTime + 90, firstTime + 1000, firstTime + 2000, firstTime + 2030, firstTime + 2060};
 
-    @Test(groups="fast")
-    public void testBasicEncodeDecode() throws Exception {
         final byte[] compressedTimes = TimelineCoder.compressTimes(unencodedTimes);
-        System.out.printf("Compressed times: %s\n", new String(Hex.encodeHex(compressedTimes)));
+        //System.out.printf("Compressed times: %s\n", new String(Hex.encodeHex(compressedTimes)));
         final int[] decompressedTimes = TimelineCoder.decompressTimes(compressedTimes);
         Assert.assertEquals(decompressedTimes.length, unencodedTimes.length);
-        for (int i=0; i<unencodedTimes.length; i++) {
+        for (int i = 0; i < unencodedTimes.length; i++) {
+            Assert.assertEquals(decompressedTimes[i], unencodedTimes[i]);
+        }
+    }
+
+    @Test(groups = "fast")
+    public void testRepeats() throws Exception
+    {
+        final int firstTime = 1293846;
+        final int[] unencodedTimes = new int[]{firstTime, firstTime + 5, firstTime + 5, firstTime + 5, firstTime + 1000, firstTime + 1000, firstTime + 2030, firstTime + 2060};
+
+        final byte[] compressedTimes = TimelineCoder.compressTimes(unencodedTimes);
+        final int[] decompressedTimes = TimelineCoder.decompressTimes(compressedTimes);
+        Assert.assertEquals(decompressedTimes.length, unencodedTimes.length);
+        for (int i = 0; i < unencodedTimes.length; i++) {
             Assert.assertEquals(decompressedTimes[i], unencodedTimes[i]);
         }
     }