ning/Arecibo

commit dfb3053859f2137c520f6bf2dc926e097f121301

collector: add 2 healthcheck endpoints
Add a healthcheck for the DAO and the TimelineEventHandler.

This required to make the EmbeddedJettyJerseyModule accept urlpatterns,
as the MetricsServlet doesn't go through Guice.

Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

alert/src/main/java/com/ning/arecibo/alert/AreciboAlertService.java
@@ -16,7 +16,7 @@
 
 package com.ning.arecibo.alert;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
@@ -163,7 +163,7 @@ public class AreciboAlertService
                 }
             },
             new AlertDataModule(),
-            new EmbeddedJettyJerseyModule(ImmutableList.<String>of("com.ning.arecibo.util.jaxrs")),
+            new EmbeddedJettyJerseyModule(ImmutableMap.<String, String>of("/xn/rest/.*", "com.ning.arecibo.event.receiver,com.ning.arecibo.util.jaxrs")),
             new RESTEventReceiverModule(AlertEventProcessor.class, "arecibo.alert:name=AlertEventProcessor"),
             new UDPEventReceiverModule(),
             new AggregatorClientModule(),

collector/src/main/java/com/ning/arecibo/collector/EventCollectorServer.java
@@ -16,7 +16,7 @@
 
 package com.ning.arecibo.collector;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
@@ -124,7 +124,10 @@ public class EventCollectorServer
     {
         final Injector injector = Guice.createInjector(Stage.PRODUCTION,
             new LifecycleModule(new StagedLifecycle()),
-            new EmbeddedJettyJerseyModule(ImmutableList.<String>of("com.ning.arecibo.collector.resources", "com.ning.arecibo.util.jaxrs")),
+            new EmbeddedJettyJerseyModule(ImmutableMap.<String, String>of(
+                "/xn/rest/.*", "com.ning.arecibo.event.receiver",
+                "/rest/.*", "com.ning.arecibo.collector.resources,com.ning.arecibo.util.jaxrs"
+            )),
             new CollectorRESTEventReceiverModule(),
             new UDPEventReceiverModule(),
             new MBeanModule(),

util/src/main/java/com/ning/arecibo/util/EmbeddedJettyJerseyModule.java
@@ -16,9 +16,7 @@
 
 package com.ning.arecibo.util;
 
-import com.google.common.base.Joiner;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.servlet.ServletModule;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
@@ -28,22 +26,22 @@ import org.eclipse.jetty.servlet.DefaultServlet;
 import org.skife.config.ConfigurationObjectFactory;
 
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
 public class EmbeddedJettyJerseyModule extends ServletModule
 {
     private static final String STATIC_URL_PATTERN = "/static/*";
 
-    private final List<String> resources = Lists.newArrayList("com.ning.arecibo.event.receiver");
+    private final Map<String, String> resources = new HashMap<String, String>();
 
     public EmbeddedJettyJerseyModule()
     {
-        this(ImmutableList.<String>of());
+        this(ImmutableMap.<String, String>of());
     }
 
-    public EmbeddedJettyJerseyModule(final List<String> resources)
+    public EmbeddedJettyJerseyModule(final Map<String, String> resources)
     {
-        this.resources.addAll(resources);
+        this.resources.putAll(resources);
     }
 
     @Override
@@ -58,12 +56,13 @@ public class EmbeddedJettyJerseyModule extends ServletModule
         bind(DefaultServlet.class).asEagerSingleton();
         serve(STATIC_URL_PATTERN).with(DefaultServlet.class);
 
-        final String jerseyResources = Joiner.on(",").join(resources);
-        serve("*").with(GuiceContainer.class, new HashMap<String, String>()
-        {
+        for (final String urlPattern : resources.keySet()) {
+            serveRegex(urlPattern).with(GuiceContainer.class, new HashMap<String, String>()
             {
-                put(PackagesResourceConfig.PROPERTY_PACKAGES, jerseyResources);
-            }
-        });
+                {
+                    put(PackagesResourceConfig.PROPERTY_PACKAGES, resources.get(urlPattern));
+                }
+            });
+        }
     }
 }

collector/src/test/java/com/ning/arecibo/collector/TestModulesFactory.java
@@ -16,6 +16,7 @@
 
 package com.ning.arecibo.collector;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
 import com.google.inject.name.Names;
@@ -69,7 +70,10 @@ public class TestModulesFactory implements IModuleFactory
                 bind(String.class).annotatedWith(Names.named(TEST_JETTY_HOST)).toInstance("127.0.0.1");
                 bind(Integer.class).annotatedWith(Names.named(TEST_JETTY_PORT)).toInstance(port);
                 install(new LifecycleModule());
-                install(new EmbeddedJettyJerseyModule());
+                install(new EmbeddedJettyJerseyModule(ImmutableMap.<String, String>of(
+                    "/xn/rest/.*", "com.ning.arecibo.event.receiver",
+                    "/rest/.*", "com.ning.arecibo.collector.resources,com.ning.arecibo.util.jaxrs"
+                )));
                 install(new UDPEventReceiverModule());
                 install(new RMIModule());
                 install(new CollectorTestModule());

collector/pom.xml
@@ -87,11 +87,22 @@
         <dependency>
             <groupId>com.google.inject.extensions</groupId>
             <artifactId>guice-multibindings</artifactId>
-            <scope>runtime</scope>
         </dependency>
         <dependency>
             <groupId>com.google.inject.extensions</groupId>
             <artifactId>guice-servlet</artifactId>
+        </dependency>
+        <dependency>
+            <groupId>com.yammer.metrics</groupId>
+            <artifactId>metrics-core</artifactId>
+        </dependency>
+        <dependency>
+            <groupId>com.yammer.metrics</groupId>
+            <artifactId>metrics-guice</artifactId>
+        </dependency>
+        <dependency>
+            <groupId>com.yammer.metrics</groupId>
+            <artifactId>metrics-servlet</artifactId>
         </dependency>
         <dependency>
             <groupId>commons-io</groupId>
@@ -258,7 +269,9 @@
                             <excludes>
                                 <exclude>.gitignore</exclude>
                                 <exclude>src/main/resources/collector.sql</exclude>
-                                <exclude>src/main/resources/com/ning/arecibo/collector/persistent/TimelineAggregatorDAO.sql.stg</exclude>
+                                <exclude>
+                                    src/main/resources/com/ning/arecibo/collector/persistent/TimelineAggregatorDAO.sql.stg
+                                </exclude>
                             </excludes>
                         </configuration>
                     </execution>

collector/src/main/java/com/ning/arecibo/collector/guice/CollectorModule.java
@@ -22,6 +22,9 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Key;
 import com.google.inject.Module;
 import com.google.inject.TypeLiteral;
+import com.google.inject.multibindings.Multibinder;
+import com.ning.arecibo.collector.healthchecks.DAOHealthCheck;
+import com.ning.arecibo.collector.healthchecks.TimelineEventHandlerHealthCheck;
 import com.ning.arecibo.collector.persistent.TimelineAggregator;
 import com.ning.arecibo.collector.persistent.TimelineEventHandler;
 import com.ning.arecibo.collector.process.EventHandler;
@@ -38,6 +41,9 @@ import com.ning.arecibo.util.service.ServiceLocator;
 import com.ning.arecibo.util.timeline.TimelineDAO;
 import com.ning.arecibo.util.timeline.persistent.FileBackedBuffer;
 import com.ning.jersey.metrics.TimedResourceModule;
+import com.yammer.metrics.core.HealthCheck;
+import com.yammer.metrics.guice.InstrumentationModule;
+import com.yammer.metrics.reporting.guice.MetricsServletModule;
 import org.skife.config.ConfigurationObjectFactory;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.IDBI;
@@ -124,6 +130,12 @@ public class CollectorModule extends AbstractModule
 
     protected void configureStats()
     {
+        install(new MetricsServletModule("/1.0/healthcheck", "/1.0/metrics", "/1.0/ping", "/1.0/threads"));
+        final Multibinder<HealthCheck> healthChecksBinder = Multibinder.newSetBinder(binder(), HealthCheck.class);
+        healthChecksBinder.addBinding().to(TimelineEventHandlerHealthCheck.class).asEagerSingleton();
+        healthChecksBinder.addBinding().to(DAOHealthCheck.class).asEagerSingleton();
+
+        install(new InstrumentationModule());
         install(new TimedResourceModule());
     }
 

collector/src/main/java/com/ning/arecibo/collector/healthchecks/DAOHealthCheck.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright 2010-2012 Ning, Inc.
+ *
+ * Ning licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.ning.arecibo.collector.healthchecks;
+
+import com.google.inject.Inject;
+import com.ning.arecibo.util.timeline.TimelineDAO;
+import com.yammer.metrics.core.HealthCheck;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DAOHealthCheck extends HealthCheck
+{
+    private final Logger log = LoggerFactory.getLogger(DAOHealthCheck.class);
+
+    private final TimelineDAO dao;
+
+    @Inject
+    public DAOHealthCheck(final TimelineDAO dao)
+    {
+        this.dao = dao;
+    }
+
+    @Override
+    public String name()
+    {
+        return DAOHealthCheck.class.getSimpleName();
+    }
+
+    @Override
+    public Result check() throws Exception
+    {
+        try {
+            dao.test();
+        }
+        catch (Exception e) {
+            log.warn("{} check failed", name());
+            return Result.unhealthy(e);
+        }
+
+        log.info("{} check succeeded", name());
+        return Result.healthy();
+    }
+}

collector/src/main/java/com/ning/arecibo/collector/healthchecks/TimelineEventHandlerHealthCheck.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright 2010-2012 Ning, Inc.
+ *
+ * Ning licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.ning.arecibo.collector.healthchecks;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.ning.arecibo.collector.persistent.TimelineEventHandler;
+import com.ning.arecibo.event.MapEvent;
+import com.yammer.metrics.core.HealthCheck;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Singleton
+public class TimelineEventHandlerHealthCheck extends HealthCheck
+{
+    private static final String EVENT_TYPE = "AreciboCollectorHealthCheck";
+    private static final UUID EVENT_UUID = UUID.randomUUID();
+
+    private final Logger log = LoggerFactory.getLogger(DAOHealthCheck.class);
+
+    private final TimelineEventHandler processor;
+
+    @Inject
+    public TimelineEventHandlerHealthCheck(final TimelineEventHandler eventHandler)
+    {
+        this.processor = eventHandler;
+    }
+
+    @Override
+    public String name()
+    {
+        return TimelineEventHandlerHealthCheck.class.getSimpleName();
+    }
+
+    @Override
+    public Result check() throws Exception
+    {
+        try {
+            final Map<String, Object> payload = ImmutableMap.<String, Object>of(
+                "short", Short.MAX_VALUE,
+                "int", Integer.MAX_VALUE,
+                "long", Long.MAX_VALUE,
+                "float", Float.MAX_VALUE,
+                "double", Double.MAX_VALUE
+            );
+            processor.handle(new MapEvent(System.currentTimeMillis(), EVENT_TYPE, EVENT_UUID, payload));
+        }
+        catch (Exception e) {
+            log.warn("{} check failed", name());
+            return Result.unhealthy(e);
+        }
+
+        log.info("{} check succeeded", name());
+        return Result.healthy();
+    }
+}

commit 22839f032c5a0e3502dbe0e1e5822b6294bdc987

util: fix broken EmbeddedJettyJerseyModule
dfb3053859f2137c520f6bf2dc926e097f121301 broke the /rest/1.0 endpoints.

Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

alert/src/main/java/com/ning/arecibo/alert/AreciboAlertService.java
@@ -16,7 +16,7 @@
 
 package com.ning.arecibo.alert;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableList;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
@@ -163,7 +163,7 @@ public class AreciboAlertService
                 }
             },
             new AlertDataModule(),
-            new EmbeddedJettyJerseyModule(ImmutableMap.<String, String>of("/xn/rest/.*", "com.ning.arecibo.event.receiver,com.ning.arecibo.util.jaxrs")),
+            new EmbeddedJettyJerseyModule("/xn/rest/.*", ImmutableList.<String>of("com.ning.arecibo.event.receiver", "com.ning.arecibo.util.jaxrs")),
             new RESTEventReceiverModule(AlertEventProcessor.class, "arecibo.alert:name=AlertEventProcessor"),
             new UDPEventReceiverModule(),
             new AggregatorClientModule(),

collector/src/main/java/com/ning/arecibo/collector/EventCollectorServer.java
@@ -16,7 +16,7 @@
 
 package com.ning.arecibo.collector;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableList;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
@@ -124,10 +124,11 @@ public class EventCollectorServer
     {
         final Injector injector = Guice.createInjector(Stage.PRODUCTION,
             new LifecycleModule(new StagedLifecycle()),
-            new EmbeddedJettyJerseyModule(ImmutableMap.<String, String>of(
-                "/xn/rest/.*", "com.ning.arecibo.event.receiver",
-                "/rest/.*", "com.ning.arecibo.collector.resources,com.ning.arecibo.util.jaxrs"
-            )),
+            new EmbeddedJettyJerseyModule("(.)*/rest/.*",
+                ImmutableList.<String>of("com.ning.arecibo.event.receiver",
+                    "com.ning.arecibo.collector.resources",
+                    "com.ning.arecibo.util.jaxrs"
+                )),
             new CollectorRESTEventReceiverModule(),
             new UDPEventReceiverModule(),
             new MBeanModule(),

util/src/main/java/com/ning/arecibo/util/EmbeddedJettyJerseyModule.java
@@ -16,7 +16,8 @@
 
 package com.ning.arecibo.util;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
 import com.google.inject.servlet.ServletModule;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
@@ -25,23 +26,26 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.skife.config.ConfigurationObjectFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class EmbeddedJettyJerseyModule extends ServletModule
 {
     private static final String STATIC_URL_PATTERN = "/static/*";
 
-    private final Map<String, String> resources = new HashMap<String, String>();
+    private final String urlPatterns;
+    private final List<String> resources = new ArrayList<String>();
 
     public EmbeddedJettyJerseyModule()
     {
-        this(ImmutableMap.<String, String>of());
+        this("/xn/rest/.*", ImmutableList.<String>of("com.ning.arecibo.event.receiver"));
     }
 
-    public EmbeddedJettyJerseyModule(final Map<String, String> resources)
+    public EmbeddedJettyJerseyModule(final String urlPatterns, final List<String> resources)
     {
-        this.resources.putAll(resources);
+        this.urlPatterns = urlPatterns;
+        this.resources.addAll(resources);
     }
 
     @Override
@@ -56,13 +60,11 @@ public class EmbeddedJettyJerseyModule extends ServletModule
         bind(DefaultServlet.class).asEagerSingleton();
         serve(STATIC_URL_PATTERN).with(DefaultServlet.class);
 
-        for (final String urlPattern : resources.keySet()) {
-            serveRegex(urlPattern).with(GuiceContainer.class, new HashMap<String, String>()
+        serveRegex(urlPatterns).with(GuiceContainer.class, new HashMap<String, String>()
+        {
             {
-                {
-                    put(PackagesResourceConfig.PROPERTY_PACKAGES, resources.get(urlPattern));
-                }
-            });
-        }
+                put(PackagesResourceConfig.PROPERTY_PACKAGES, Joiner.on(",").join(resources));
+            }
+        });
     }
 }

collector/src/test/java/com/ning/arecibo/collector/TestModulesFactory.java
@@ -16,7 +16,7 @@
 
 package com.ning.arecibo.collector;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableList;
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
 import com.google.inject.name.Names;
@@ -70,10 +70,11 @@ public class TestModulesFactory implements IModuleFactory
                 bind(String.class).annotatedWith(Names.named(TEST_JETTY_HOST)).toInstance("127.0.0.1");
                 bind(Integer.class).annotatedWith(Names.named(TEST_JETTY_PORT)).toInstance(port);
                 install(new LifecycleModule());
-                install(new EmbeddedJettyJerseyModule(ImmutableMap.<String, String>of(
-                    "/xn/rest/.*", "com.ning.arecibo.event.receiver",
-                    "/rest/.*", "com.ning.arecibo.collector.resources,com.ning.arecibo.util.jaxrs"
-                )));
+                new EmbeddedJettyJerseyModule("(.)*/rest/.*",
+                    ImmutableList.<String>of("com.ning.arecibo.event.receiver",
+                        "com.ning.arecibo.collector.resources",
+                        "com.ning.arecibo.util.jaxrs"
+                    ));
                 install(new UDPEventReceiverModule());
                 install(new RMIModule());
                 install(new CollectorTestModule());